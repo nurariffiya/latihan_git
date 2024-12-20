745e7593f30f7704edd997f9341591685c96f8b7
unknown <nurariffiyatulfuraida.com>
2024-12-20 10:02:26 +0700

    first commit add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Git</title>
+</head>
+<body>
+    <h1>Latihan Git</h1>
+    <button>Submit</button>
+</body>
+</html>
